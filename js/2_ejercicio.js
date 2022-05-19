87df6ac8c26f967cd29f20085593de6eea8eab91
nicolas andrade <nicolasandrade328@gmail.com>
2022-05-19 12:14:20 -0300

    agregamos la funcionalidad de introducir un numero valido

--- js/2_ejercicio.js
+++ js/2_ejercicio.js
@@ -11,27 +11,29 @@ Luego mostrar la calificación en un alert según los siguientes rangos de nota:
 Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”.
  Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
  */
+console.log(NaN);
+let nota = parseInt(prompt("ingrese una nota de 0 a 10"));
+if (Number.isNaN(nota) == false) {  //number.isNAN(nota) me devuelve true cuando el valor ingresado es una letra.
+  if (nota >= 0 && nota <= 2) {
+    alert("Muy deficiente");
+  }
+  if (nota >= 3 && nota <= 4) {
+    alert("Insuficiente");
+  }
+  if (nota >= 5 && nota <= 6) {
+    alert("Suficiente");
+  }
+  if (nota == 7) {
+    alert("Bien");
+  }
+  if (nota >= 8 && nota <= 9) {
+    alert("Notable");
+  }
+  if (nota == 7) {
+    alert("Sobresaliente");
+  }
+  if (nota <= 0 || nota > 10) {
+    document.write("numero erroneo");
+  }
+} else alert("introduce un nuemero valido");
 
- let nota =parseInt( prompt('ingrese una nota de 0 a 10'));
-if (nota>=0 || nota<=2){
-    alert('Muy deficiente');
-}
-if (nota>=3 || nota<=4){
-    alert('Insuficiente');
-}
-if (nota>=5 || nota<=6){
-    alert('Suficiente');
-}
-if(nota==7){
-    alert('Bien');
-}
-if (nota>=8 || nota<=9){
-    alert('Notable');
-}
-if(nota==7){
-    alert('Sobresaliente');
-}
-if(nota<=0 || nota>10){
-    document.write('numero erroneo')
-}
-/*duda con lo de nuimero no valido*/
