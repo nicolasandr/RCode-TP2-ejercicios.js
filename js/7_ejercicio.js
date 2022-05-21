024b88bfeec19a75f3cd0cf582bdc76560ce271a
nicolas andrade <nicolasandrade328@gmail.com>
2022-05-20 23:35:00 -0300

    agregado de ejercicio 7

---
+++ js/7_ejercicio.js
@@ -0,0 +1,21 @@
+/*7- Haz un script que escriba una pirámide inversa de los números del 1 al número
+que indique el usuario (no mayor de 50) de la siguiente forma : (suponiendo que
+indica 30).
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+*/
+let nota = parseInt(prompt("ingrese un numero del 1 al 50:"));
+if (nota >= 1 && nota <= 50) {
+  for (let numero = nota ; numero >= 1 ; numero--) {
+
+    for (let repetir = numero; repetir >= 1; repetir--) {
+      document.write(`${numero}`);
+    }
+    document.write(`<br>`);
+  }
+}
