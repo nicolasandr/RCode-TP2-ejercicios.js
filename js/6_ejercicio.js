3ba08a6dc58a02088b14855b1c3f710ddf233342
nicolas andrade <nicolasandrade328@gmail.com>
2022-05-20 23:06:19 -0300

    agregado de ejercicio 6

---
+++ js/6_ejercicio.js
@@ -0,0 +1,17 @@
+/*
+6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+1
+22
+333
+4444
+55555
+666666*/
+
+for(let numero = 1; numero <=30 ; numero++){
+    //document.write(`Elemento ${numero} <br>`)
+    
+    for(let repetir = 1; repetir <=numero ; repetir++){
+        document.write(`${numero}`)
+    }
+    document.write(`<br>`)
+ }
